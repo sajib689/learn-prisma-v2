9e5f51f0b2efa9bb482cc3b34f300ec0f5bd5345
sajib689 <sajibbabu751@gmail.com>
2026-05-31 20:48:14 +0600

    feat: add CRUD routes for posts in post router

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -4,5 +4,9 @@ import { postController } from "./post.controller";
 const router = Router();
 
 router.post("/", postController.createPostController);
+router.get("/", postController.getAllPostsController);
+router.get("/:id", postController.findPostByIdController);
+router.delete("/:id", postController.deletePostByIdController);
+router.put("/:id", postController.updatePostByIdController);
 
 export const postRouter = router;
