200c3eb09dd53065c8e60539ac387237bcd6ba31
sajib689 <sajibbabu751@gmail.com>
2026-06-03 22:59:40 +0600

    feat: add isFeatured filter to getAllPosts functionality in post controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -22,11 +22,13 @@ const getAllPostsController = async (req: Request, res: Response) => {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const search = (req.query.search as string) || "";
+    const isFeatured = req.query.isFeatured ? req.query.isFeatured === "true" : undefined
 
     const result = await postService.getAllPostsService({
       page,
       limit,
-      search
+      search,
+      isFeatured
     });
     res.status(200).json({
       message: "Posts fetched successfully",
